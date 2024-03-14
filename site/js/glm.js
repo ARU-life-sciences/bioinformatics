9f86214eb158635d1c6bb8df7a7dce66060b318b
Max Brown <max.carter-brown@aru.ac.uk>
2024-03-14 21:42:29 +0000

    Add poissson glm graph.

--- site/js/glm.js
+++ site/js/glm.js
@@ -1,7 +1,7 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 import * as Plot from "https://cdn.jsdelivr.net/npm/@observablehq/plot@0.6/+esm";
 
-let raw_data = await d3.csv(
+let raw_data_1 = await d3.csv(
   "https://raw.githubusercontent.com/ARU-life-sciences/bioinformatics/main/R/statistics/data/form-1__anthropometrics.csv",
   function(d) {
     // get the variable 5_Height_in_cm and change to number
@@ -12,12 +12,18 @@ let raw_data = await d3.csv(
   }
 );
 
+// remove NA's
+const raw_data = raw_data_1.filter(function(d) {
+  return d.Heart > 0;
+});
+
+
 let fitted_data = await d3.csv(
-  "https://raw.githubusercontent.com/ARU-life-sciences/bioinformatics/main/R/how_to/poisson_glm_plot_predictions/data/poisson.csv",
+  "https://raw.githubusercontent.com/ARU-life-sciences/bioinformatics/main/how_to/poisson_glm_plot_predictions/data/poisson.csv",
   function(d) {
     // get the variable 5_Height_in_cm and change to number
     return {
-      Height: +d["5_Height_in_cm"],
+      Height: +d["X5_Height_in_cm"],
       Fit: +d["fit_exp"],
       Lower: +d["lwr"],
       Upper: +d["upr"],
@@ -25,7 +31,26 @@ let fitted_data = await d3.csv(
   }
 );
 
+console.log(fitted_data);
+
 const plot = Plot.plot({
+  marginLeft: 80,
+  marginBottom: 40,
+  grid: true,
+  color: { legend: true },
+  x: {
+    inset: 6,
+    label: "Height (cm)",
+  },
+  y: {
+    label: "Heart rate (bpm)",
+  },
+  caption: "A Poisson regression of heart rate on height. Note the slight curve in the predicted fit.",
+  marks: [
+    Plot.dot(raw_data, { x: "Height", y: "Heart" }),
+    Plot.areaY(fitted_data, { x: "Height", y1: "Lower", y2: "Upper", fillOpacity: 0.3 }),
+    Plot.line(fitted_data, { x: "Height", y: "Fit" }),
+  ]
 });
 
 const div = document.querySelector("#glm_plot");
